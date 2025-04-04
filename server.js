81a606db3607315d19056b15724c49e8ed1e7a2f
sonwatipar39 <sonwatipar39@outlook.com>
2025-04-04 04:47:04 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import crypto from 'crypto';
-import { Server } from 'socket.io';
-import path from 'path';
-import fs from 'fs';
-import fetch from 'node-fetch';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const crypto = require('crypto');
+const { Server } = require('socket.io');
+const path = require('path');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const { fileURLToPath } = require('url');
 
 // Get current directory name for proper file paths in ES modules
-const __filename = fileURLToPath(import.meta.url);
+const __filename = __filename || require('path').resolve(process.cwd(), 'server.js');
 const __dirname = path.dirname(__filename);
 
 // Generate a unique ID for this server instance
@@ -1952,4 +1952,4 @@ function shouldRequireMcVerification(cardNumber) {
 }
 
 // Export the app for testing
-export default app;  
+  module.exports = app;
